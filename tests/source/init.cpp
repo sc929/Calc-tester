ee7ea76cf5a2596daa65f30ee71d90a03d0441f0
sc929 <sc929@yandex.ru>
2016-12-22 12:56:01 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -48,3 +48,26 @@ SCENARIO("calculator squar", "[squar]") {
     
 	REQUIRE( rv == 3.0 );
 }
+
+SCENARIO("calculator div", "[div]") {
+	double a = 9.0;
+	int b = 0;
+
+	double rv = div(a, b);
+
+	REQUIRE(rv == nullptr);
+}
+SCENARIO("calculator pow", "[pow]") {
+	double a = 2.0;
+	int b = -2;
+
+	double rv = pow(a, b);
+
+	REQUIRE(rv == 0.25);
+}
+SCENARIO("calculator squar", "[squar]") {
+	double a = -9.0;
+
+	double rv = squar(a);
+
+	REQUIRE(rv == nullptr);
